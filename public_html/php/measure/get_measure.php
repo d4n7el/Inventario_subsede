9ecef07bfa03dd83531244f5b046977f5d8bb22c
Daniel Felipe Zamora Ortiz <d4n7elfelipe@gmail.com>
2017-08-25 16:40:43 -0500

    eliminar sessionstar() duplicado

--- public_html/php/measure/get_measure.php
+++ public_html/php/measure/get_measure.php
@@ -1,5 +1,4 @@
 <?php
-	session_start();
 	require_once($_SERVER['DOCUMENT_ROOT'].'/php/controller/measure_controller.php');
 	$medida 			= new Measures();
 	$retorno_measure 	= $medida->get_measure();
