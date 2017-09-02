f1c43601760bf1fee77a8ac68d44e2aa10377bf1
alejandrojas <alejandrojas@github.com>
2017-09-01 20:40:45 -0500

    Arreglado

--- la_Bd_se_llama_inventarios_subsede.sql.sql
+++ la_Bd_se_llama_inventarios_subsede.sql.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 -- Base de datos: inventarios_subsede
 --
 CREATE DATABASE IF NOT EXISTS inventarios_subsede DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE 'inventarios_subsede';
+CREATE DATABASE IF NOT EXISTS `inventarios_subsede` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
