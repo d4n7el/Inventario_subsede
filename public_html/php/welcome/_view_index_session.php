fbf5758c939e80fce672386775f8b106fd4e6d5b
EstefaniaCasasYepes <30784996+EstefaniaCasasYepes@users.noreply.github.com>
2017-09-07 14:38:48 -0500

    se añadio el div de herramientas en el view_index

--- public_html/php/welcome/_view_index_session.php
+++ public_html/php/welcome/_view_index_session.php
@@ -117,6 +117,29 @@
 				</div>
 			</div>
 	    </div>
+	    <div class="col s12 targeta_inicio">
+			<div class="card transparent darken-1">
+				<div class="card-content white-text">
+					<div class="title" >
+						<span class="card-title">Herramientas</span>
+					</div>
+					<div class="content  teal lighten-1 centrar" >
+						<div class="izquierda col s6 centrar">
+							<i class="material-icons">shopping_cart</i>
+						</div>
+						<div class="derecha col s6 centrar">
+							10
+						</div>
+					</div>
+				</div>
+				<div class="card-action teal darken-2" >
+					<a href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/php/tools/create_tools.php" id="" class="link_page">Ingresar Herramienta <i class="material-icons right">add</i></a>
+				</div>
+				<div class="card-action teal darken-2" >
+					<a href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/php/tools" class="link_page">Ver Herramientas<i class="material-icons right">visibility</i></a>
+				</div>
+			</div>
+	    </div>
     </div>
     <div class="col s9 offset-s3" id="vista_ventana">
     	
