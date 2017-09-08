30f7035ddd03407fc175942ec51e3cee6581aec9
alejandrojas <alejandrojas@github.com>
2017-09-07 19:44:19 -0500

    linea del nombre base de datos

--- la_Bd_se_llama_inventarios_subsede.sql.sql
+++ la_Bd_se_llama_inventarios_subsede.sql.sql
@@ -19,8 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: inventarios_subsede
 --
-CREATE DATABASE IF NOT EXISTS inventarios_subsede DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
 CREATE DATABASE IF NOT EXISTS `inventarios_subsede` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `inventarios_subsede`;
 
 -- --------------------------------------------------------
 
